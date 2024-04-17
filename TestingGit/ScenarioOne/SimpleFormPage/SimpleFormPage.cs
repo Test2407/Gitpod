3d23917cb55fea71aa1c2d10d4039fc5858a74d0
theo <theotech24@gmail.com>
2024-04-17 13:14:38 +0530

    Add project files.

---
+++ TestingGit/ScenarioOne/SimpleFormPage/SimpleFormPage.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.ScenarioOne.SimpleFormPag
+{
+    public class SimpleFormPage
+    {
+        private IPage page;
+        private ILocator MessageInput;
+        private ILocator GetCheckedbtn;
+        private ILocator Message;
+
+        public ILocator MessageOutput { get => Message; set => Message = value; }
+
+        public SimpleFormPage(IPage _page) {
+            this.page = _page;
+            this.MessageInput = _page.GetByPlaceholder("Please enter your Message");
+            this.GetCheckedbtn = _page.GetByRole(AriaRole.Button, new()
+            {
+                Name = "Get Checked Value"
+            });
+            this.Message = _page.Locator("#message");
+        }
+        public async Task GotoSimpleFormPage()
+        {
+            await page.GotoAsync("https://www.lambdatest.com/selenium-playground/");
+            await page.GetByRole(AriaRole.Link, new() { Name= "Simple Form Demo" }).ClickAsync();
+        }
+        public async Task EnterMessage(String message)
+        {
+            await  MessageInput.FillAsync(message);
+        }
+        public async  Task ClickCheckBtn()
+        {
+            await GetCheckedbtn.ClickAsync();
+        }
+
+        public async Task<String> getMessage()
+        {
+            var message = Message.TextContentAsync();
+            Console.WriteLine(message);
+            return await message;
+        }
+
+        public async Task<String> getCurrentURL()
+        {
+            return page.Url;
+        }
+    }
+}
---
+++ TestingGit/ScenarioOne/Tests/TestScenarioOneWindows.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.ScenarioOne.SimpleFormPag;
+
+namespace TestProject.ScenarioOne.Tests
+{
+    public class TestScenarioOneWindows:PageTest
+    {
+        private SimpleFormPage page;
+        public LambdaTestConfig config;
+        public IPage resPage;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            config = new LambdaTestConfig();
+            String resUrl = config.GetURL(1);
+
+            var browser = await Playwright.Chromium.ConnectAsync(resUrl);
+            resPage = await browser.NewPageAsync();
+            page = new SimpleFormPage(resPage);
+            await page.GotoSimpleFormPage();
+        }
+        [Test]
+        public async Task ValidateURL()
+        {
+            var URL = await page.getCurrentURL();
+            if (URL.Contains("simple-form-demo"))
+            {
+                await LambdaTestConfig.SetTestStatus("passed", "URL Present", resPage);
+            }
+            else
+            {
+                await LambdaTestConfig.SetTestStatus("failed", "URL Invalid", resPage);
+                throw new Exception("Fail");
+
+            }
+
+        }
+
+        [Test]
+        public async Task ValidateMessage()
+        {
+            var InputMessage = "Welcome to LambdaTest";
+            await page.EnterMessage(InputMessage);
+            await page.ClickCheckBtn();
+            var textContent = await page.MessageOutput.TextContentAsync();
+            if (textContent.Contains(InputMessage))
+            {
+                await LambdaTestConfig.SetTestStatus("passed", "Message Found", resPage);
+            }
+            else
+            {
+                await LambdaTestConfig.SetTestStatus("failed", "Message Not Found", resPage);
+                throw new Exception("Fail");
+            }
+        }
+
+    }
+}
