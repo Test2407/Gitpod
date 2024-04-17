9fc33b173f68a0de2dde65117b6e276b188c1eee
theo <theotech24@gmail.com>
2024-04-17 08:17:57 +0530

    Add project files.

---
+++ TestDocker/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestDocker
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
